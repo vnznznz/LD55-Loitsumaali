33dbf918c663f4e13ec6ba7ab61454410ee7f16b
Vinzenz Sinapius <Vinzenz.Sinapius@gmail.com>
2024-04-14 21:56:41 +0200

    icon

--- export.sh
+++ export.sh
@@ -8,6 +8,9 @@ GODOT_PATH="$HOME/work/tools/godot/Godot_v3.5.3-stable_x11.64"
 
 
 $GODOT_PATH --no-window --export "HTML5"
+$GODOT_PATH --no-window --export "Linux"
+$GODOT_PATH --no-window --export "Mac"
+$GODOT_PATH --no-window --export "Windows"
 
 # delete zip if it exists
 if [ -f ./export/web.zip ]; then
@@ -18,4 +21,7 @@ zip -r ./web.zip ./web/
 cd ..
 
 
-$HOME/.config/itch/apps/butler/butler push export/web.zip vnznz/ld55:web-export
+$HOME/.config/itch/apps/butler/butler push export/web.zip vnznz/ld55-loitsumaali:web-export
+$HOME/.config/itch/apps/butler/butler push export/linux vnznz/ld55-loitsumaali:linux
+$HOME/.config/itch/apps/butler/butler push export/windows vnznz/ld55-loitsumaali:windows
+$HOME/.config/itch/apps/butler/butler push export/mac vnznz/ld55-loitsumaali:mac
